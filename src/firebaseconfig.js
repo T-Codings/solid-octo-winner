12253cd9a5c9de3ffac83fab34348699a2b94d14
THEODORE <ndi.theo@yahoo.com>
2025-11-21 10:59:28 -0800

    Added landing page and initial signin and singup

---
+++ src/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYhecfMd5XgIKnhCMCdM6DUJpgCQ7WPSE",
+  authDomain: "first-project-a211e.firebaseapp.com",
+  projectId: "first-project-a211e",
+  storageBucket: "first-project-a211e.firebasestorage.app",
+  messagingSenderId: "315203969457",
+  appId: "1:315203969457:web:769ee616f122abf2bb9d90",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
