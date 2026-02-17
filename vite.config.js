a4f62829be18b7999d786ce2bf45f813258e8351
THEODORE <ndi.theo@yahoo.com>
2026-02-16 18:56:09 -0800

    files edited

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/solid-octo-winner/", // your repo name
+  base: "/solid-octo-winner/", // must match the folder/repo name
 });
