57ebd8765f98a9c8332f1c3bfea29fa87539ec2a
THEODORE <ndi.theo@yahoo.com>
2026-02-23 18:42:51 -0800

    files edited

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,10 @@ import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { useAuth } from "../context/AuthContext";
 import ContactList from "./ContactList";
+import ChattiesLogo from "../assets/Chatties.png";
+import MingcuteChat from "../assets/mingcutechat.png";
+import CreateNewChat from "../assets/Createnewchat.png";
+import {SearchIcon} from "lucide-react";
 
 function Sidebar({ onSelectContact, readContacts = [] }) {
   const { currentUser } = useAuth();
@@ -42,7 +46,27 @@ function Sidebar({ onSelectContact, readContacts = [] }) {
   return (
     <div className="w-full md:w-[340px] lg:w-[400px] border-r border-gray-300 h-[320px] md:h-full overflow-y-auto bg-white">
       <div className="p-4">
-        <h2 className="text-lg font-bold mb-4">Contacts</h2>
+        {/* Top logos */}
+        <div className="flex items-center gap-2 mb-4">
+          <img src={ChattiesLogo} alt="Chatties" className="w-24 h-6 " />
+          <img src={MingcuteChat} alt="Mingcute Chat" className="w-8 h-8 object-contain" />
+        </div>
+
+        {/* Search bar */}
+        <div className="flex items-center mb-2">
+          <div className="flex flex-1 items-center bg-white border border-gray-300 rounded-[14px] px-2 py-1">
+            <SearchIcon className="text-gray-400 w-5 h-5 mr-2" />
+            <input
+              type="text"
+              placeholder="Search people, contacts..."
+              className="flex-1 bg-white outline-none text-lg text-gray-700 py-1 "
+            />
+          </div>
+          <img src={CreateNewChat} alt="Create New Chat" className="w-7 h-7 object-contain ml-3 cursor-pointer" />
+        </div>
+        
+
+     
         {loading && <div>Loading contacts...</div>}
         {error && <div className="text-red-500">{error}</div>}
         <ContactList
