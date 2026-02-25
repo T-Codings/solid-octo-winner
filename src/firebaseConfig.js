7188f49bd528c2cfc6c04f6f92943e9138a74ea7
THEODORE <ndi.theo@yahoo.com>
2026-02-24 18:20:23 -0800

    firebaseConfigedited

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,16 +5,20 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDNB_FM-v33OOo46ntwFNeYDIyHW-CQfbU",
-  authDomain: "chat-application-e174b.firebaseapp.com",
-  projectId: "chat-application-e174b",
-  storageBucket: "chat-application-e174b.firebasestorage.app",
-  messagingSenderId: "967304951560",
-  appId: "1:967304951560:web:8398a1dd2e23d2dd595ebd",
-  measurementId: "G-Q3YEQYP51L",
+  apiKey: "AIzaSyDTNylnxQT0pSQ6OxkBsG_q4sRAPkXxFoM",
+  authDomain: "chatty-ba61d.firebaseapp.com",
+  projectId: "chatty-ba61d",
+  storageBucket: "chatty-ba61d.firebasestorage.app",
+  messagingSenderId: "1002464337849",
+  appId: "1:1002464337849:web:a51e4ceca0886571e5a068",
+  measurementId: "G-RBN91166SN"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
